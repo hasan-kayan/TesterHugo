a29ad04af0e593088ad369f6861ac461bd455a05
hasan-kayan <hasankayan2000@hotmail.com>
2024-05-19 15:28:10 +0300

    Unsaved File

---
+++ include/structure_creator/structure.h
@@ -0,0 +1,11 @@
+#ifndef STRUCTURE_H
+#define STRUCTURE_H
+
+#include <string>
+
+class ProjectStructureCreator {
+public:
+    int createProjectBase(const std::string& projectName);
+};
+
+#endif // STRUCTURE_H
