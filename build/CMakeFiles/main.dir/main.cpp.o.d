6452fd95198afe3f35f51025d9a6c9d916c0c543
hasan-kayan <hasankayan2000@hotmail.com>
2024-07-08 13:03:01 +0300

    Clear

--- build/CMakeFiles/main.dir/main.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/main.dir/main.cpp.o: \
-  /Users/hasankayan/Desktop/Personal/TesterHugo/main.cpp \
-  /Users/hasankayan/Desktop/Personal/TesterHugo/./include/structure_creator/structure.h
--- main.cpp
+++
@@ -1,7 +0,0 @@
-#include "./include/structure_creator/structure.h"
-
-int main() {
-    ProjectStructureCreator creator;
-    creator.createProjectBase("MyProject");
-    return 0;
-}
